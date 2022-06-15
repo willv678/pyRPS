a7027b11254444e5c94a4cb398445490f8a9ec52
Will <willv678@gmail.com>
2022-06-15 13:49:38 -0400

    1.0!
    Game working and completed!

--- rps.py
+++ rps.py
@@ -4,35 +4,174 @@ Created on Wed Jun 15 09:20:14 2022
 
 @author: wvarner3
 """
-
+#0 is loose, tie is 1, win is 2
 import random
 import time
 import os
 
-def RANDOM():
-    return(random.randint(1, 3))
-
+choiceEnemy = "ROROAROWo"
 scoreP = 0
 scoreM = 0
-choice = 0
-enemy = 11
-choiceEnemy = "choice enemy"
 choiceName = "null"
 hands = ""
 
+
+def RANDOM():
+    return(random.randint(1, 3))
+
 def enemyRoll():
+    global enemy
     enemy = RANDOM()
-    print(enemy)
-    
-    
+    global hands
+    global choiceEnemy
+    #print(enemy)
+    if enemy == 1:
+        choiceEnemy = "rock"
+        #print("rock")
+    elif enemy == 2:
+        choiceEnemy = "paper"
+        #print("paper")
+    elif enemy == 3:
+        choiceEnemy = "scissors"
+        #print("scissors")
+def handStuff():      
+    global choiceEnemy
+    global enemy
+    global choice
+    global status
+    global choiceName
+    #print("Guess what",choiceEnemy)
+    if choiceName == "rock":
+        #print("IT SHOWS ROCK!")
+        if enemy == 1:
+            status = 1
+
+            print ( """
+    _______               _______  
+---'   ____)             (____   '---
+      (_____)          (_____) 
+      (_____)          (_____) 
+      (____)            (____)   
+---.__(___)              (___)__.---
+
+            _____ _      _ 
+           |_   _(_) ___| |
+             | | | |/ _ \ |
+             | | | |  __/_|
+             |_| |_|\___(_)
+            """)
+        elif enemy == 2:
+            status = 0
+            print ( """
+    _______                   _______  
+---'   ____)             ____(____    '---
+      (_____)          (_____) 
+      (_____)          (_____) 
+      (____)            (____)   
+---.__(___)              (___________.---
+            """)
+        elif enemy == 3:
+            status = 2
+            print ( """
+    _______                   _______  
+---'   ____)             ____(____    '---
+      (_____)          (_____) 
+      (_____)          (_____) 
+      (____)                 (____)   
+---.__(___)                  (____)_.---
+            """)
+
+    elif choiceName == "paper":
+        if enemy == 1:
+            choiceEnemy ="rock"
+            status = 3
+            print ( """
+     _______                  _______  
+---'    ____)____           (____   '---
+           ______)        (_____) 
+          _______)        (_____) 
+         _______)          (____)   
+---.__________)             (___)__.---
+            """)
+        elif enemy == 2:
+            status = 1
+            print( """
+     _______                      _______  
+---'    ____)____             ___(____   '---
+           ______)         (_______ 
+          _______)        (_________ 
+         _______)          (_________   
+---.__________)              (_________.---     
+                             
+            _____ _      _ 
+           |_   _(_) ___| |
+             | | | |/ _ \ |
+             | | | |  __/_|
+             |_| |_|\___(_)                            
+            """)
+        elif enemy == 3:
+            choiceEnemy = "scissors"
+            status = 1
+            print( """
+     _______                     _______  
+---'    ____)____          _____(____   '---
+           ______)        (_______ 
+          _______)        (__________ 
+         _______)               (____)
+---.__________)                  (___)__.---           
+            """)
+    elif choiceName == "scissors":
+        #print("ok we good?")
+        if enemy == 1:
+            choiceEnemy = "rock"
+            status = 0
+            print( """
+    _______                 _______    
+---'   ____)____           (____   '---
+          ______)         (_____) 
+       __________)        (_____) 
+      (____)               (____)
+---.__(___)                 (___)__.---
+            """)
+        elif enemy == 2:
+            choiceEnemy= "paper"
+            status = 2
+            print ( """
+    _______                     ________     
+---'   ____)____           ____(____    '---
+          ______)         (______           
+       __________)        (_______      
+      (____)               (_______         
+---.__(___)                  (__________.---
+            """)
+        elif enemy == 3:
+            choiceEnemy = "scissors"
+            status = 1
+            print ( """
+    _______                      _______     
+---'   ____)____           _____(____   '---
+          ______)         (_______                 
+       __________)        (__________       
+      (____)                    (____)      
+---.__(___)                      (___)__.---
+
+            _____ _      _ 
+           |_   _(_) ___| |
+             | | | |/ _ \ |
+             | | | |  __/_|
+             |_| |_|\___(_)
+                 
+
+            """)
+        #print("HUGE DRAMA")
 def score():
     print("        Current Score: ", scoreP, " - ", scoreM)
 
 
     
 def showHands():
-    print(hands)
-    print("You played", choiceName, "cpu played", choiceEnemy)
+    #print("showhands formula which is BEFORE the hand show command, so they rolled a", choiceName)
+    print("   You played :", choiceName, "\n   Enemy played :", choiceEnemy, "\n")
     
     
 def prompt():
@@ -43,14 +182,33 @@ def prompt():
           """)
     
 
+def changeScore():
+    global scoreP
+    global scoreM
+    global status
+    if status == 0:
+        print("You loose!")
+        scoreM = scoreM + 1
+    elif status == 1:
+        print ("It's a tie!")
+    elif status == 2:
+        print ("You win!")
+        scoreP = scoreP + 1
+        
+    
+
 print("""
-░█▀▄░█▀█░█▀▀░█░█░░░█▀█░█▀█░█▀█░█▀▀░█▀▄░░░█▀▀░█▀▀░▀█▀░█▀▀░█▀▀░█▀█░█▀▄░█▀▀
-░█▀▄░█░█░█░░░█▀▄░░░█▀▀░█▀█░█▀▀░█▀▀░█▀▄░░░▀▀█░█░░░░█░░▀▀█░▀▀█░█░█░█▀▄░▀▀█
-░▀░▀░▀▀▀░▀▀▀░▀░▀░░░▀░░░▀░▀░▀░░░▀▀▀░▀░▀░░░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀
+  ____            _      ____                         ____       _                        _ 
+ |  _ \ ___   ___| | __ |  _ \ __ _ _ __   ___ _ __  / ___|  ___(_)___ ___  ___  _ __ ___| |
+ | |_) / _ \ / __| |/ / | |_) / _` | '_ \ / _ \ '__| \___ \ / __| / __/ __|/ _ \| '__/ __| |
+ |  _ < (_) | (__|   <  |  __/ (_| | |_) |  __/ |     ___) | (__| \__ \__ \ (_) | |  \__ \_|
+ |_| \_\___/ \___|_|\_\ |_|   \__,_| .__/ \___|_|    |____/ \___|_|___/___/\___/|_|  |___(_)
+                                   |_|                                                      
                                   """)
 
 while True:
-    score()
+    global choice
+    global status
     prompt()
     choice = input("Enter choice(1/2/3): ")
     print("\n")
@@ -85,133 +243,10 @@ while True:
       (____)
 ---.__(___)
         """
-    enemyRoll()        
-        #enemy stuff below vvvvv
-        
-    if choice == 1:
-        if enemy == 1:
-            choiceEnemy = "rock"
-            hands = """
-    _______               _______  
----'   ____)             (____   '---
-      (_____)          (_____) 
-      (_____)          (_____) 
-      (____)            (____)   
----.__(___)              (___)__.---
-
-            _____ _      _ 
-           |_   _(_) ___| |
-             | | | |/ _ \ |
-             | | | |  __/_|
-             |_| |_|\___(_)
-            """
-        elif enemy == 2:
-            choiceEnemy = "paper"
-            hands = """
-    _______                   _______  
----'   ____)             ____(____    '---
-      (_____)          (_____) 
-      (_____)          (_____) 
-      (____)            (____)   
----.__(___)              (___________.---
-            """
-        elif enemy == 3:
-            choiceEnemy = "scissors"
-            hands = """
-    _______                   _______  
----'   ____)             ____(____    '---
-      (_____)          (_____) 
-      (_____)          (_____) 
-      (____)                 (____)   
----.__(___)                  (____)_.---
-            """
-        print(choiceEnemy)
-    if choice == '2':
-        if enemy == '1':
-            choiceEnemy ="rock"
-            hands = """
-     _______                  _______  
----'    ____)____           (____   '---
-           ______)        (_____) 
-          _______)        (_____) 
-         _______)          (____)   
----.__________)             (___)__.---
-            """
-        elif enemy == '2':
-            choiceEnemy ="paper"
-            hands= """
-     _______                      _______  
----'    ____)____             ___(____   '---
-           ______)         (_____) 
-          _______)        (_____) 
-         _______)          (____)   
----.__________)              (_________.---     
-                             
-            _____ _      _ 
-           |_   _(_) ___| |
-             | | | |/ _ \ |
-             | | | |  __/_|
-             |_| |_|\___(_)                            
-            """
-        elif enemy == '3':
-            choiceEnemy = "scissors"
-            hands= """
-     _______                     _______  
----'    ____)____          _____(____   '---
-           ______)        (_______ 
-          _______)        (__________ 
-         _______)               (____)
----.__________)                  (___)__.---           
-            """
-    print(hands)
-    if choice == '3':
-        if enemy == '1':
-            choiceEnemy = "rock"
-            hands= """
-    _______                 _______    
----'   ____)____           (____   '---
-          ______)         (_____) 
-       __________)        (_____) 
-      (____)               (____)
----.__(___)                 (___)__.---
-            """
-        elif enemy == '2':
-            choiceEnemy= "paper"
-            hands = """
-    _______                     ________     
----'   ____)____           ____(____    '---
-          ______)         (______           
-       __________)        (_______      
-      (____)               (_______         
----.__(___)                  (__________.---
-            """
-        elif enemy == '3':
-            choiceEnemy = "scissors"
-            hands = """
-    _______                      _______     
----'   ____)____           _____(____   '---
-          ______)         (_______                 
-       __________)        (__________       
-      (____)                    (____)      
----.__(___)                      (___)__.---
-
-            _____ _      _ 
-           |_   _(_) ___| |
-             | | | |/ _ \ |
-             | | | |  __/_|
-             |_| |_|\___(_)
-                 
-
-            """
-            
-            
-
-            
-            
-            
+    
     print("Selected", choiceName )
     print(hand)
-    os.system('pause')
+    #os.system('pause')
     print("\n" * 3)
     time.sleep(1)
     print("Rock... \n")
@@ -221,5 +256,11 @@ while True:
     print("Scissors... \n")
     time.sleep(.5)
     print("Shoot!")
+    enemyRoll()
     print("\n" * 2)
-    showHands()  
+    handStuff()
+    showHands()
+    changeScore()
+    os.system('pause')
+    print("\n" * 50)
+    score()
